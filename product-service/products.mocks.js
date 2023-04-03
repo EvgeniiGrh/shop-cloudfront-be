ea212dc78094f72906877f5724665a8f0d6adc75
Yauheni Harokh <yauheni_harokh@epam.com>
2023-04-03 23:42:14 +0300

    create getProductsList and getProductById functions

---
+++ product-service/products.mocks.js
@@ -0,0 +1,38 @@
+module.exports.productsList = [
+    {
+      title: 'Triumph Bonneville T120',
+      price: 12000,
+      imgUrl: 'https://images.triumphmotorcycles.co.uk/media-library/images/motorcycles/modern-classics/my21/t120/family%20page%20images/t120-family-timeline-slide-engine-1920x780.jpg',
+      id: 1
+    },
+    {
+      title: 'Triumph Thruxton RS',
+      price: 16900,
+      imgUrl: 'https://cdp.azureedge.net/products-private/prod/99fdabe4-cbc0-4cae-9c46-fbe88ecfc4f3/790f309b-4eb8-4866-853f-af1701743e13/00000000-0000-0000-0000-000000000000/2cc45f91-3480-44fc-9c07-ad8101676add/7741bace-03e3-4f9c-b098-af2100fc6ff4/6000000005.jpg',
+      id: 2
+    },
+    {
+      title: 'Triumph Scrambler 1200',
+      price: 14745,
+      imgUrl: 'https://images.triumphmotorcycles.co.uk/media-library/images/motorcycles/modern-classics/my21/dr-ds-d74%20-%20scrambler%201200/family%20page%20images/scarmbler-1200-family-hero-1920x1080.jpg',
+      id: 3
+    },
+    {
+      title: 'Triumph Speed Twin 1200',
+      price: 12500,
+      imgUrl: 'https://images.triumphmotorcycles.co.uk/media-library/images/motorcycles/modern-classics/my21/dd4%20speed%20twin/family%20page%20images/speed-twin-family-timeline-handling-1920x780.jpg',
+      id: 4
+    },
+    {
+      title: 'Triumph Bonneville SpeedMaster',
+      price: 13700,
+      imgUrl: 'https://ultimatemotorcycling.com/wp-content/uploads/2021/02/2022-Triumph-Bonneville-Speedmaster-First-Look-cruiser-retro-motorcycle-featured.jpg',
+      id: 5
+    },
+    {
+      title: 'Triumph Bonneville Bobber',
+      price: 13500,
+      imgUrl: 'https://triumph.granmoto.ru/user/pages/02.motorcycles/04.classic/09.Bonneville-bobber/06._key_features/bobber_details_9.jpg',
+      id: 6
+    },
+  ];
